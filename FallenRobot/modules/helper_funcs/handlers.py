e470d989b65ec8e62bcd201cb30134bdffd53c4f
Zein <122434261+zeinn1@users.noreply.github.com>
2023-02-20 20:31:10 +0200

    Update handlers.py

--- FallenRobot/modules/helper_funcs/handlers.py
+++ FallenRobot/modules/helper_funcs/handlers.py
@@ -12,9 +12,9 @@ import FallenRobot.modules.sql.blacklistusers_sql as sql
 from FallenRobot import ALLOW_EXCL, DEMONS, DEV_USERS, DRAGONS, TIGERS, WOLVES
 
 if ALLOW_EXCL:
-    CMD_STARTERS = ("", "!")
+    CMD_STARTERS = ("/", "!")
 else:
-    CMD_STARTERS = ""
+    CMD_STARTERS = "/"
 
 
 class AntiSpam:
